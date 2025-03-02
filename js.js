80ce8201848ecd8206a88586ea7447b83647ca39
Yurii1010 <yuriipechko@gmail.com>
2025-03-02 17:13:41 +0800

    Кредо от Альберта

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Новые дороги появляются только в пути!')
+	alert('Двери позитивных изменений открываются изнутри!')
 })
